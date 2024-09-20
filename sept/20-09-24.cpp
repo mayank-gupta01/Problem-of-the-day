2575f3ec336de1caa0c288d74c5b648e4aaccf89
Mayank Gupta <mayank.gupta09022001@gmail.com>
2024-09-20 13:35:18 +0530

    solved

---
+++ sept/20-09-24.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int length = s.length();
+        if (length == 0) {
+            return s;
+        }
+
+        // Find the longest palindromic prefix
+        int left = 0;
+        for (int right = length - 1; right >= 0; right--) {
+            if (s[right] == s[left]) {
+                left++;
+            }
+        }
+
+        // If the whole string is a palindrome, return the original string
+        if (left == length) {
+            return s;
+        }
+
+        // Extract the suffix that is not part of the palindromic prefix
+        string nonPalindromeSuffix = s.substr(left);
+        cout<<"nonPalindromeSuffix: "<<nonPalindromeSuffix<<endl;
+        string reverseSuffix =
+            string(nonPalindromeSuffix.rbegin(), nonPalindromeSuffix.rend());
+
+        cout<<"reverseSuffix: "<<reverseSuffix<<endl;
+        // Form the shortest palindrome by prepending the reversed suffix
+        
+        return reverseSuffix + shortestPalindrome(s.substr(0, left)) +
+               nonPalindromeSuffix;
+    }
+};
